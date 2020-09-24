f9f131fd403d7091495052433b79651cf07856b7
luisinhoLayme <71614685+luisinhoLayme@users.noreply.github.com>
2020-09-24 15:35:36 -0400

    Completo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,52 @@
 //Variables
-const boton = document.getElementById('btn-burger');
+const boton_abrir = document.getElementById('btn-burger-abrir');
+const boton_cerrar = document.getElementById('btn-burger-cerrar');
 const menu = document.querySelector('.menu');
-const contenedor_texto = document.querySelector('.contenedor-texto');
+const menu_botones = document.querySelectorAll('.menu a');
+const ipad = window.matchMedia('screen and (max-width: 526px)');
 
 //Hide Show menu Burger
-boton.addEventListener('click', menuHide);
+menuBotones();
+desktop(ipad);
+ipad.addListener(desktop);
+
+
+// Funcion para validar el width de ventana
+function desktop(event) {
+	if(event.matches) {
+		boton_abrir.addEventListener('click', menuHide);
+		boton_cerrar.addEventListener('click', menuHide);
+	} else {
+		// menu.classList.remove('active');
+	}
+}
 
 // Funcion que hace la funcion al menu
 function menuHide() {
 	if(menu.classList.contains('active')) {
 		menu.classList.remove('active');
-		contenedor_texto.style.margin = '149px 0';
+		boton_cerrar.style.display = 'none';
 	} else {
 		menu.classList.add('active');
-		contenedor_texto.style.margin = '173px 0';
+		boton_cerrar.style.display = 'block';
+	}
+}
+// Funcion para los botones del menhu
+function menuBotones() {
+	menu_botones.forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			menu.classList.remove('active');
+			boton_cerrar.style.display = 'none';
+		})
+	});
+}
+
+// Function para controlar burger menu
+window.onresize = () => {
+	let ancho = screen.width;
+	console.log(ancho);
+	if (ancho > 526) {
+		menu.classList.remove('active');
+		boton_cerrar.style.display = 'none';
 	}
 }
